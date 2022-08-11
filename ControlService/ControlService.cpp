3af8b42724a1a99e767084f4f70cbd64b269c3b8
NyanBanan <mokshin2002nikita@gmail.com>
2022-08-11 21:20:08 +0300

    version system updated, handmade hash function added

--- ControlService/ControlService.cpp
+++ ControlService/ControlService.cpp
@@ -18,7 +18,7 @@ void ControlService::uninitialize()
 int ControlService::main(const std::vector<std::string>& args){
     {
         ControlTaskController control(new Control);
-        log_information(control.getControlledPointer()->decodeWriteHash(control.getControlledPointer()->handWriteHash("PocoAgent001")));
+        //log_information(control.getControlledPointer()->decodeWriteHash(control.getControlledPointer()->handWriteHash("PocoAgent001")));
         control.startControlTask();
         waitForTerminationRequest();
     }
